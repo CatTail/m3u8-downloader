7b272f27801515b2487d36c5ea7c1bdf47ce75a1
zhongchiyu <zhongchiyu@gmail.com>
2017-04-20 18:21:45 +0800

    chore: add distribute script

--- renderer.js
+++ renderer.js
@@ -5,18 +5,21 @@ const execSync = require('child_process').execSync
 const request = require('request-promise-native')
 const m3u8 = require('m3u8')
 
-const bin = joinPath(__dirname, 'bin', os.platform() + '_' + os.arch())
-
-try {
-  fs.accessSync(bin)
-} catch (err) {
-  log('不支持该平台')
-}
+const isDevelopment = process.env.NODE_ENV === 'development'
+const resourcesPath = joinPath(isDevelopment ? __dirname : process.resourcesPath, 'bin')
+const bin = joinPath(resourcesPath, os.platform() + '_' + os.arch())
 
 const sourceElement = document.getElementById('source')
 const targetElement = document.getElementById('target')
 const downloadElement = document.getElementById('download')
 const logElement = document.getElementById('log')
+
+try {
+  log('使用 ffmpeg 文件', bin)
+  fs.accessSync(bin)
+} catch (err) {
+  log('不支持该平台')
+}
 
 downloadElement.addEventListener('click', (event) => {
   const source = sourceElement.value
@@ -33,7 +36,12 @@ downloadElement.addEventListener('click', (event) => {
       fetchTsList(m3u).then((buf) => {
         fs.writeFileSync(joinPath(target, '/all.ts'), buf)
         log('成功合并')
-        execSync(`${bin} -y -i ${concatPath} -bsf:a aac_adtstoasc -vcodec copy ${convertPath}`)
+        try {
+          execSync(`${bin} -y -i ${concatPath} -bsf:a aac_adtstoasc -vcodec copy ${convertPath}`)
+        } catch (err) {
+          log(err.stack, err.message, err.name)
+          throw err
+        }
         log('成功转码')
       })
     })
