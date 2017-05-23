bae6032d72335edda0c7c9214bd32a9eb16437ab
zhongchiyu <zhongchiyu@gmail.com>
2017-05-23 14:55:22 +0800

    feat: add menu

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# m3u8 downloader
+> Download and convert m3u8 into mp4 file
+
+## build
+
+    npm run dist
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -7,24 +7,112 @@ const url = require('url')
 let win
 
 function createWindow () {
-  // Create the browser window.
+    // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
+    // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  // win.webContents.openDevTools()
+  const template = [
+    {
+      label: 'Edit',
+      submenu: [
+                {role: 'undo'},
+                {role: 'redo'},
+                {type: 'separator'},
+                {role: 'cut'},
+                {role: 'copy'},
+                {role: 'paste'},
+                {role: 'pasteandmatchstyle'},
+                {role: 'delete'},
+                {role: 'selectall'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+                {role: 'reload'},
+                {role: 'forcereload'},
+                {role: 'toggledevtools'},
+                {type: 'separator'},
+                {role: 'resetzoom'},
+                {role: 'zoomin'},
+                {role: 'zoomout'},
+                {type: 'separator'},
+                {role: 'togglefullscreen'}
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+                {role: 'minimize'},
+                {role: 'close'}
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://electron.atom.io') }
+        }
+      ]
+    }
+  ]
 
-  // Emitted when the window is closed.
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+                {role: 'about'},
+                {type: 'separator'},
+                {role: 'services', submenu: []},
+                {type: 'separator'},
+                {role: 'hide'},
+                {role: 'hideothers'},
+                {role: 'unhide'},
+                {type: 'separator'},
+                {role: 'quit'}
+      ]
+    })
+
+        // Edit menu
+    template[1].submenu.push(
+            {type: 'separator'},
+      {
+        label: 'Speech',
+        submenu: [
+                    {role: 'startspeaking'},
+                    {role: 'stopspeaking'}
+        ]
+      }
+        )
+
+        // Window menu
+    template[3].submenu = [
+            {role: 'close'},
+            {role: 'minimize'},
+            {role: 'zoom'},
+            {type: 'separator'},
+            {role: 'front'}
+    ]
+  }
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+    // Open the DevTools.
+    // win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
     win = null
   })
 }
@@ -36,16 +124,16 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
